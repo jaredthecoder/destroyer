843b59e022011c4b33c73763015e88eecb17e662
jaredmichaelsmith <jared@jaredsmith.io>
2015-09-06 18:00:50 -0500

    Add basic Facebook service integration

---
+++ destroyer/services/facebook.py
@@ -0,0 +1,21 @@
+"""facebook.py - Module for Facebook service functionality"""
+
+import time
+
+import click
+
+
+class FacebookDestroyer():
+    """Destroyer class for Facebook integration.
+    """
+    def __init__(self):
+        """Initializer method"""
+        self.logger = None
+
+    def _unfriend(self):
+        """Private method that takes a Facebook friend object and unfriends them."""
+        pass
+
+    def destroy(self):
+        """Public method that implements the abstracted functionality of unfriending users"""
+        pass
