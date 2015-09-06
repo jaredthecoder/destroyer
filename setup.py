8741252a367ed3e55d1edc3dac4fd3a057f44905
jaredmichaelsmith <jared@jaredsmith.io>
2015-09-06 16:06:26 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""setup.py - Distutils setup file"""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='destroyer',
+    version='0.1.0'
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        destroyer=destroyer.destroyer:cli
+    ''',
+)
