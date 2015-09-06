cd07a033abe1195085cbd4d112583f7558ecf77a
jaredmichaelsmith <jared@jaredsmith.io>
2015-09-06 11:48:14 -0500

    Add sample settings file

---
+++ destroyer/settings.sample.py
@@ -0,0 +1,10 @@
+"""settings.py - Module for storing settings for destroyer"""
+
+
+# Put your Twitter API keys and secrets here
+# Make a Twitter app at dev.twitter.com and add the API keys here.
+# Be sure to give your app both read and write access.
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
